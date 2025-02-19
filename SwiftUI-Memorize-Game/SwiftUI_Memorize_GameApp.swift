ca2fbbcecfbb8c9fbee84d605e2f65339e79f2f2
Bohdan Dovhal <dovhalwork@gmail.com>
2025-02-19 12:23:31 +0200

    Initial Commit

---
+++ SwiftUI-Memorize-Game/SwiftUI_Memorize_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Memorize_GameApp.swift
+//  SwiftUI-Memorize-Game
+//
+//  Created by Bohdan Dovhal on 19.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Memorize_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
